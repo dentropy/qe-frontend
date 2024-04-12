b8ccc57f7f80032b3effac09a2617510a6da7351
Paul Mullins <paulmullins@protonmail.com>
2024-04-11 20:21:06 -0400

    Able to detect window.nostr

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,25 @@
+import * as react from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [NIP07, setNIP07] = useState(false)
+  react.useEffect(() =>{
+    if(Object.keys(window).includes("nostr")){
+      setNIP07(true)
+    }
+  })
 
+  if(NIP07){
+    return(
+      <>
+        <h1>NIP07 is Enabed</h1>
+      </>
+    )
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>NIP07 is not enabled get your NSEC ready</h1>
     </>
   )
 }
